a74c811430584fc715a486ec1d0c44c7b7a81997
Benji Hannam <benjamin.c.hannam.18@dartmouth.edu>
2016-08-16 14:50:46 -0400

    Started on the mazestruct

--- mazestruct.c
+++ mazestruct.c
@@ -1,1 +1,86 @@
-// A structure file to hold the available information about the maze
+/* 
+* A structure file to hold the available information about the maze
+*
+* Created by Benji Hannam, 2016 for use in Team Core Dumped in a Maze for CS50
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+/************************* global variables ******************/
+
+/************************* local types **********************/
+typedef struct spot {
+	bool north;
+	bool south;
+	bool east;
+	bool west;
+} spot_t;
+
+/************************* global types *********************/
+
+typedef struct mazestruct{
+	spot_t **map;
+	int height;
+	int width;
+	int num_avatars;
+
+} mazestruct_t;
+
+
+/********************** global functions *********************/
+
+/**************** maze_new() ****************/
+mazestruct_t* maze_new(int height, int width, int num_avatars){
+
+	mazestruct_t *new_maze = malloc(sizeof(mazestruct_t));
+	if(new_maze == NULL){
+		printf("Error allocating memory to new maze struct, exiting..\n");
+		exit(2);
+	}
+	//initialize the map of the maze
+	spot_t* temp[width][height];
+
+	//insert all the spots into the map
+	for (int i = 0; i < width; i++){
+		for (int j = 0; j < height; j++){
+
+			spot_t *new_spot = malloc(sizeof(spot_t));
+			if(new_spot == NULL){
+				printf("Error allocaitng memory to a new spot struct, exiting..\n");
+				exit(2);
+			}
+			temp[i][j] = new_spot;
+		}
+	}
+
+	//insert the walls on the north edge
+	for (int i = 0; i < width; i++){
+		temp[i][0]->north = true;
+	}
+
+	//insert the walls on the south edge
+	for (int i = 0; i < width; i++){
+		temp[i][height - 1]->south = true;
+	}
+	//insert the walls on the east edge
+	for (int i = 0; i < height; i++){
+		temp[width -1][i]->east = true;
+	}
+
+	//insert the walls on the west edge
+	for (int i = 0; i < height; i++){
+		temp[0][i]->west = true;
+	}
+
+	new_maze->map = temp;
+	new_maze->height = height;
+	new_maze->width = width;
+	new_maze->num_avatars = num_avatars;
+
+	return new_maze;
+
+
+}
---
+++ mazetest.c
@@ -0,0 +1,16 @@
+/* 
+* A test file for mazestruct.c
+*
+* Created by Benji Hannam, 2016 for use in Team Core Dumped in a Maze for CS50
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include "mazestruct.c"
+
+int main(int argc, char *argv[]){
+
+	mazestruct_t* new_maze = maze_new(1,1,1);
+}
